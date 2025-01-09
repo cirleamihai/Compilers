ac7372cefe0b85872519cbfbd2157df6c4558003
krlea_dev <alexcirlea@hotmail.com>
2025-01-09 15:01:30 +0200

    Added grammar parsing

---
+++ Lab 6/grammar.py
@@ -0,0 +1,38 @@
+def load_grammar(filename):
+    """
+    Reads grammar rules from a file (e.g., rules.in) of the form:
+        E -> T E'
+        E' -> + T E' | e
+        ...
+    Returns a dictionary like:
+        {
+            'E':  [['T', "E'"]],
+            "E'": [['+', 'T', "E'"], ['e']],
+            ...
+        }
+    """
+    grammar = {}
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+
+            if not line or '->' not in line:
+                continue
+
+            non_terminal, terminals = line.split('->')
+            non_terminal = non_terminal.strip()
+
+            raw_productions = terminals.split('|')
+            productions = []
+
+            for production in raw_productions:
+                production = production.strip()
+                symbol = production.split()
+                productions.append(symbol)
+
+            grammar[non_terminal] = productions
+
+        return grammar
+
+print(load_grammar('rules.in'))
